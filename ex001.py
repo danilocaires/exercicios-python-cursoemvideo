4f914b6dcd01bfa98221f86122675fe7c1e0b1c3
Danilo Caires Martinez Vieira <danilocaires@outlook.com>
2023-05-22 20:38:23 -0300

    Cleaning up GitHub tests

--- ex001.py
+++ ex001.py
@@ -1,8 +1,3 @@
 # Crie um programa que escreve "Olá Mundo" na tela
 
 print('Olá Mundo!')
-
-# Teste de alteração de arquivo no GitHub
-# Teste de alteração de arquivo no GitHub Desktop
-# Teste de alteração de arquivo no VSCode
-#
